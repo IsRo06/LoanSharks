53087374b79a5b00ad27c57cfd27dcb60f241d18
Jeevan Munnangi <jrmunnangi@gmail.com>
2024-04-08 11:12:33 -0400

    mid

--- CLIENT/src/pages/Signin.jsx
+++ CLIENT/src/pages/Signin.jsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
 import styles from './Signin.module.css'
 import { useState } from 'react'
+import logo from '../images/logo.png'
 
 
 
@@ -22,12 +24,30 @@ export default function Signin(){
 
     return(
     <div className={styles.background}>
-        <div className={styles.mainbox}>
-            <input className={styles.usernamebox} value={username} onChange={usernameFill} placeholder="Username"></input>
-            <input className={styles.passwordbox} value={password} onChange={passwordFill} placeholder="Password"></input>
-            <button onClick={signin}>Submit</button>
-            
+        <div className='largeBox'>
+            <div className='leftSide'>
+                <div className='backimage'>
+                    <Link to="/">
+                    <img id={styles.logoImage} src={logo} alt=""/>
+                    </Link>
+                </div>
+            </div>
+            <div className='middle'>
+                <div className={styles.mainbox}>
+                    <input className={styles.usernamebox} value={username} onChange={usernameFill} placeholder="Username"></input>
+                    <input className={styles.passwordbox} value={password} onChange={passwordFill} placeholder="Password"></input>
+                    <button onClick={signin}>Submit</button>
+                    
+                </div>
+            </div>
+            <div className='rightSide'>
+
+            </div>
         </div>
+        
+        
+        
     </div>
+    
     )
   }
