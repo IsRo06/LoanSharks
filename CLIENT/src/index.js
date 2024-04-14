5924199eefcd864ea2990f7676ef4cb8bcf34a6f
Isabella Roman <Isabella Roman>
2024-04-14 15:26:44 -0400

    fixed service worker

--- CLIENT/src/index.js
+++ CLIENT/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
-import App from './App.js';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import ApolloProvider from './ApolloProvider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
+ReactDOM.render(ApolloProvider, document.getElementById('root'));
+serviceWorker.unregister();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
