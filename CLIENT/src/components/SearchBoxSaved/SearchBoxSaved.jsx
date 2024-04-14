56149d8254270cd3bc4bf272257b82738afea0fb
Lennon King <116830939+lennon-king@users.noreply.github.com>
2024-04-14 14:51:56 -0400

    started car results page

---
+++ CLIENT/src/components/SearchBoxSaved/SearchBoxSaved.jsx
@@ -0,0 +1,90 @@
+import React, {useState} from 'react'
+import { Link } from "react-router-dom";
+import styles from './SearchBoxSaved.module.css'
+import Dropdown from '../Dropdowns/Dropdowns.jsx';
+import OurCalendar from '../Calendar/Calendar.jsx';
+import calandarIcon from '../../images/calandar-icon.jpg';
+import clockIcon  from '../../images/clock-icon.jpg';
+import locationIcon from '../../images/location-icon.jpg';
+
+export default function SearchBoxSaved(){
+  const locations = [ 'Gainesville', 'Orlando', 'Miami', 'Tallahassee', 'Tampa'];
+  const times = ['12:00AM', '1:00AM', '2:00AM', '3:00AM', '4:00AM', '5:00AM', '6:00AM', '7:00AM', 
+                '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', 
+                '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM', '10:00PM', '11:00PM',  ]
+
+  const [pickupLocation, setpickupLocation] = useState("");
+  const [pickupDate, setpickupDate] = useState([]);
+  const [pickupTime, setpickupTime] = useState("");
+  const [dropoffDate, setdropoffDate] = useState([]);
+  const [dropoffTime, setdropoffTime] = useState("");
+
+  return(
+    <div id={styles.searchBoxSaved}>
+      <div id={styles.pickUpLocation}>
+        <img src={locationIcon} alt=""/>
+        <div className={styles.searchBoxSavedText}>
+          <label>Pick-up</label>
+          <div className={styles.dropDown} style={{zIndex: 50}}>
+            <Dropdown name="Pick-up Location" options={locations} arrow="↓"/>
+          </div>
+        </div>
+      </div>
+
+      <div id={styles.pickUpLocation}>
+        <img src={locationIcon} alt=""/>
+        <div className={styles.searchBoxSavedText}>
+          <label>Pick-up</label>
+          <div className={styles.dropDown} style={{zIndex: 50}}>
+            <Dropdown name="Pick-up Location" options={locations} arrow="↓"/>
+          </div>
+        </div>
+      </div>
+      
+      <div id={styles.dateAndTime}>
+        <div className={styles.dateAndTimeContainer}>
+          <img src={calandarIcon} alt=""/>
+          <div className={styles.searchBoxSavedText}>
+            <label>Pick-up Date</label>
+            <div className={styles.calendar} style={{zIndex:40}}>
+              <OurCalendar name="Pick-up Date" arrow="↓"/>
+            </div>
+          </div>
+        </div>
+        
+        <div className={styles.dateAndTimeContainer}>
+          <img src={clockIcon} alt=""/>
+          <div className={styles.searchBoxSavedText}>
+            <label>Pick-up Time</label>
+            <div className={styles.dropDown} style={{zIndex:30}}>
+              <Dropdown name="Pick-up Time" options={times} arrow="↓"/>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.dateAndTimeContainer}>
+          <img src={calandarIcon} alt=""/>
+          <div className={styles.searchBoxSavedText}>
+            <label>Drop-off Date</label>
+            <div className={styles.calendar} style={{zIndex:20}}>
+              <OurCalendar name="Drop-off Date" arrow="↓"/>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.dateAndTimeContainer}>
+          <img src={clockIcon} alt=""/>
+          <div className={styles.searchBoxSavedText}>
+            <label>Drop-off Time</label>
+            <div className={styles.dropDown} style={{zIndex:10}}>
+              <Dropdown name="Drop-off Time" options={times} arrow="↓"/>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <Link to="/cars" id={styles.searchBtn}><button id={styles.buttonText}>Search</button></Link>
+    </div>
+  );
+
+}
