7dc2f8190116bb28198aaffdaab209186eeb11ed
Isabella Roman <Isabella Roman>
2024-04-14 19:55:45 -0400

    Added chatbox

--- CLIENT/src/index.js
+++ CLIENT/src/index.js
@@ -4,13 +4,8 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
-
+import { render } from "react-dom";
+render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
