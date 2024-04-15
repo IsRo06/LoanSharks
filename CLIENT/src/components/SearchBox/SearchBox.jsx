748897b754af949462f706617bc848f1dedbeeb4
Stephanie Fong <fong.s@ufl.edu>
2024-04-14 22:41:36 -0400

    search box has proper logic

--- CLIENT/src/components/SearchBox/SearchBox.jsx
+++ CLIENT/src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useContext} from 'react'
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { locationContext, rentalRangeContext } from '../../App.js';
 import styles from './SearchBox.module.css'
 import Dropdown from '../Dropdowns/Dropdowns.jsx';
@@ -11,20 +11,27 @@ import locationIcon from '../../images/location-icon.jpg';
 export default function SearchBox(){
   const [location, setLocation] = useContext(locationContext);
   const [rentalRange, setRentalRange] = useContext(rentalRangeContext);
-const [rentalTimes, setRentalTimes] = useState(["",""]);
+  const [rentalTimes, setRentalTimes] = useState(["0","0"]);
+
+  const navigate = useNavigate();
 
   const locations = [ 'Gainesville', 'Orlando', 'Miami', 'Tallahassee', 'Tampa'];
   const times = ['12:00AM', '1:00AM', '2:00AM', '3:00AM', '4:00AM', '5:00AM', '6:00AM', '7:00AM', 
                 '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', 
-                '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM', '10:00PM', '11:00PM',  ]
+                '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM', '10:00PM', '11:00PM']
 
   function handleSearch() {
-    console.log()
-    if ((rentalRange.toString() === "0,0,0,0" || rentalTimes.toString() === ",") || location === "None"){
+    if (rentalRange.includes(0) || rentalTimes.includes("0") || location === "None"){
       window.alert("Please fill out all the search boxes")
     }
+    else if ((rentalRange[0] > rentalRange[2]) || ((rentalRange[0] === rentalRange[2]) && (rentalRange[1] > rentalRange[3])) ) {
+      window.alert("Drop-off Date is before Pick-up Date");
+    }
+    else if (times.indexOf(rentalTimes[0]) > times.indexOf(rentalTimes[1])) {
+      window.alert("Drop-off Time is before Pick-up Time");
+    }
     else {
-      window.alert("Success!")
+      navigate('/cars')
     }
   }
 
