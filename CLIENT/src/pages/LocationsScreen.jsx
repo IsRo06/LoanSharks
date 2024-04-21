6bbc8fd7193e78a7a9f89edeab53a07668872c01
Stephanie Fong <fong.s@ufl.edu>
2024-04-21 18:36:18 -0400

    fixed map placement

--- CLIENT/src/pages/LocationsScreen.jsx
+++ CLIENT/src/pages/LocationsScreen.jsx
@@ -4,40 +4,14 @@ import FloridaIMg from "../images/Map_of_Florida_Regions.png"
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
-
-
-
-export default function LocationsScreen(){
-
-    
+export default function LocationsScreen(){    
     return(
-        
-        <div className={styles.outerBox}>
+        <>
             <Header/>
-            <div className={styles.space}/>
-            <div className={styles.secondBox}>
+            <div id={styles.imageContainer}>
                 <img id={styles.stateImg} src={FloridaIMg} alt=""/>
-    
             </div>
             <Footer/>
-        </div>
+        </>
     )
 }
-
-
-                // <MapProvider
-                //     width={800}
-                //     height={400}
-                //     projection="geoAzimuthalEqualArea"
-                //     projectionConfig={{ rotate: [-10.0, -53.0, 0], scale: 1200,}}
-                //     >
-                //     <svg viewBox="0 0 800 400">
-                //         <Geographies geography={geoUrl}>
-                //             {({ geographies }) =>
-                //                 geographies.map((geo) => (
-                //                     <Geography key={geo.rsmKey} geography={geo} fill="#EAEAEC" stroke="#D6D6DA" />
-                //             ))
-                //             }
-                //         </Geographies> 
-                //     </svg>
-                // </MapProvider>
